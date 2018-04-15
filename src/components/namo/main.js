0a2eed64b6fca9e714ee850bf506920bc450a14e
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-15 11:32:10 +0530

    axios

--- src/components/namo/main.js
+++ src/components/namo/main.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import ComponentView from './view';
 
-export default class Main extends React.Component{
-    render(){
+export default class Main extends React.Component {
+    componentDidMount() {
+        axios('http://localhost:5000/namo').then((res) => {
+            console.log(res);
+        })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+    render() {
         return (ComponentView.bind(this))();
     }
 }
