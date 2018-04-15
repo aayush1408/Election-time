c7c1dce397acbf2534d26dbfc6f34c967cf23dd2
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-15 15:41:52 +0530

    imgs added

--- src/components/namo/view.js
+++ src/components/namo/view.js
@@ -4,7 +4,10 @@ import "materialize-css";
 import 'materialize-css/dist/css/materialize.min.css';
 const view = function () {
     return (
-        <h2>Narendra Modi</h2>
+        <div>
+            <img className="circle img-responsive" src="https://pbs.twimg.com/profile_images/718314968102367232/ypY1GPCQ_400x400.jpg" />
+            <h2>Narendra Modi</h2>
+        </div>
     );
 };
 
--- src/components/rahul/view.js
+++ src/components/rahul/view.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './style.css';
-
-const view = function(){
+import "materialize-css";
+import 'materialize-css/dist/css/materialize.min.css';
+const view = function () {
     return (
-        <h2>Rahul Gandhi</h2>
+        <div>
+            <img className="circle img-responsive" src="https://pbs.twimg.com/profile_images/974851878860312582/O-Zn2b72_400x400.jpg" />
+            <h2>Rahul Gandhi</h2>
+        </div>
     );
 };
 
