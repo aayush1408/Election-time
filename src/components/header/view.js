196edcc2a4969ed01cc9ce268635bea99b57175d
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-17 00:20:27 +0530

    header style changed

--- src/components/header/view.js
+++ src/components/header/view.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles.css';
-
+import "materialize-css";
+import 'materialize-css/dist/css/materialize.min.css';
 var view = function () {
 	return (
-		<h1>Tweet War</h1>
+		<div	>
+			<h1>Tweet War</h1>
+		</div>
 	)
 }
 export default view;
